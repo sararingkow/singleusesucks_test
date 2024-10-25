68aaadbe7fdb9125fa356c527e2684f90018e49a
Sara <sape0003@stud.kea.dk>
2024-10-25 15:23:53 +0200

    Top-part and other things added

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-        require('@tailwindcss/typography'),
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/aspect-ratio'),
-        require('@tailwindcss/container-queries'),
-    ],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        lato: ["Lato", "sans-serif"],
+        kanit: ["Kanit", "sans-serif"],
+      },
+      cursor: {
+        handcursor: "url(/cursor.png) 100 100, pointer",
+      },
+      animation: {
+        "infinite-scroll": "infinite-scroll 45s linear infinite",
+      },
+      keyframes: {
+        "infinite-scroll": {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100%)" },
+        },
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/container-queries"),
+  ],
+};
